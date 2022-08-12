9327d38ef0887cfb0f4f02d309ef6a21251bf869
sujk912 <aa991122aa@gmail.com>
2022-08-13 06:27:03 +0900

    feat: Create SearchBar prototype

---
+++ components/AlocholType/SearchBar/SearchBar.tsx
@@ -0,0 +1,11 @@
+import styles from './SearchBar.module.css';
+
+const SearchBar = () => {
+    return (
+        <div className={styles.searchBarBox}>
+            <div className={styles.searchBar}>dfsdfsdfsdf</div>
+        </div>
+    )
+};
+
+export default SearchBar;
