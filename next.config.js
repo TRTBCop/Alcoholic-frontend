0baaa30acc8d94ba4c3760afd3f04e6c5e2fb582
sujk912 <aa991122aa@gmail.com>
2022-08-13 03:30:18 +0900

    절대경로 설정 (TeletubbiesWiki 참고)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,39 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const path = require('path');
+
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.SERVER_URL}/:path*`,
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      { source: '/oauth/kakao', destination: '/auth/oauth', permanent: true },
+      { source: '/oauth/google', destination: '/auth/oauth', permanent: true },
+    ];
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+  webpack(config) {
+    config.resolve = {
+      alias: {
+        '@lib': path.resolve(__dirname, './lib'),
+        '@hooks': path.resolve(__dirname, './hooks'),
+        '@styles': path.resolve(__dirname, './styles'),
+        types: path.resolve(__dirname, './types'),
+        '@components': path.resolve(__dirname, './components'),
+        '@containers': path.resolve(__dirname, './containers'),
+      },
+      ...config.resolve,
+    };
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
