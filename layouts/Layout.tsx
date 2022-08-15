b585e187c40ec16d109a2d886351c7b0ca213c58
sujk912 <aa991122aa@gmail.com>
2022-08-16 00:58:10 +0900

    chore: Merging Layout

---
+++ layouts/Layout.tsx
@@ -0,0 +1,20 @@
+
+import styles from './Layout.module.css';
+
+const Layout = ({ children }: { children: JSX.Element | JSX.Element[] }) => {
+    return (
+        <>
+            <header className={styles.header}>
+                <section>Header</section>
+            </header>
+            <main className={styles.main}>
+                {children}
+            </main>
+            <footer className={styles.footer}>
+                <section>Footer 내용인데 이건 돌려막기 할거니까 나중에 같이 이야기 해봅시다</section>
+            </footer>
+        </>
+    );
+}
+
+export default Layout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,14 @@ import '../styles/globals.css';
 import '../styles/AlcoholHistory/AlcholHistoryCommon.css';
 
 import type { AppProps } from 'next/app';
+import Layout from '@layouts/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
