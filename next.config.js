10ec4d7debf0ff51a3d2b1be7db1203e0e2169ec
sujk912 <aa991122aa@gmail.com>
2022-08-23 18:33:12 +0900

    chore : Add scss(sass, node-sass) Library

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 module.exports = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   webpack(config) {
     config.resolve = {
       alias: {
