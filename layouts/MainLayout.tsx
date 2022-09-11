5ebdfe2f5946de97561b769835a7848ff7263140
SeunghunHan1 <hkgb0009@gmail.com>
2022-09-11 15:10:09 +0900

    [MAIN] 메인 첫페이지 작업 완료

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,11 +1,42 @@
-import styles from './Layout.module.scss';
+import styles from './MainLayout.module.scss';
+import mainLogo from '../public/assets/img/testLogo1.png';
+import { useRouter } from 'next/router';
 
 const Layout: React.FC<{ children: JSX.Element | JSX.Element[] }> = ({ children }) => {
+  const router = useRouter();
+
+  const goPage = (pathname: string) => {
+    router.push({
+      pathname,
+    });
+  };
+
   return (
     <>
-      {/* <header className={styles.header}>
-        <section>Header1</section>
-      </header> */}
+      <header className={styles.header}>
+        <div className={styles.container}>
+          <div className={styles.mainLogo}>
+            <img src={mainLogo.src} alt="" />
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <a onClick={() => goPage('/alcoholtype')}>Alcohol Type</a>
+              </li>
+              <li>
+                <a onClick={() => goPage('/alcoholhistory')}>Alcohol History</a>
+              </li>
+              <li>
+                <a onClick={() => goPage('/alcoholrecipe')}>Alcohol Recipe</a>
+              </li>
+            </ul>
+            <div className={styles.loginWrap}>
+              <button>로그인</button>
+              <button>회원가입</button>
+            </div>
+          </nav>
+        </div>
+      </header>
       <main>{children}</main>
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,23 @@
 import type { NextPage } from 'next';
 import styles from '@styles/MainPage/MainPage.module.scss';
+import layoutStyles from '@layouts/MainLayout.module.scss';
 import { FullPage, Slide } from 'react-full-page';
 import mainLogo from '../public/assets/img/testLogo.png';
+import { useRouter } from 'next/router';
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  const goPage = (pathname: string) => {
+    router.push({
+      pathname,
+    });
+  };
+
   return (
     <FullPage>
       <Slide className={styles.mainSlideItem}>
-        <div className={styles.mainSlideInner}>
+        <div className={`${styles.mainSlideInner} ${layoutStyles.container}`}>
           <div className={styles.mainLogo}>
             <img src={mainLogo.src} alt="" />
           </div>
@@ -17,8 +27,12 @@ const Home: NextPage = () => {
             작은 습관
           </h3>
           <div className={styles.mainBtnWrap}>
-            <button className={styles.mainBtn}>일지 쓰기</button>
-            <button className={styles.mainBtn}>레시피 보기</button>
+            <button className={styles.mainBtn} onClick={() => goPage('/alcoholhistory')}>
+              일지 쓰기
+            </button>
+            <button className={styles.mainBtn} onClick={() => goPage('/alcoholrecipe')}>
+              레시피 보기
+            </button>
           </div>
         </div>
       </Slide>
