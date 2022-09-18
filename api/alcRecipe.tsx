82824bcbd254e9eb4e0bff0d59e33c07b105db26
subin164 <qkrtnqls5669@gmail.com>
2022-09-18 20:16:33 +0900

    Fix: [AR] model alcRecipe fix

--- api/alcRecipe.tsx
+++ api/alcRecipe.tsx
@@ -6,9 +6,13 @@ const getAlcRecipe = () => {
   return instance.get<BaseApiResult<AlcRecipe[]>>('/api/alc-recipe');
 };
 
-const getAlcRecipeDetail = (id: number) => {
+const getAlcRecipeDetail = (id: string) => {
   return instance.get<BaseApiResult<AlcRecipeDetail>>(`/api/alc-recipe/detail/${id}`);
 };
 
+const getAlcRecipeEdit = (id: string) => {
+  return instance.get<BaseApiResult<AlcRecipeDetail>>(`/api/alc-recipe/writer/${id}`);
+};
 
-export { getAlcRecipe, getAlcRecipeDetail };
+
+export { getAlcRecipe, getAlcRecipeDetail,  getAlcRecipeEdit};
