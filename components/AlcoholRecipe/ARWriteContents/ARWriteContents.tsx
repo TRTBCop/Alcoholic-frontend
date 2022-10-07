bb69fd7c054859808de4c77789969f6c80fd34e1
subin164 <qkrtnqls5669@gmail.com>
2022-10-08 03:03:02 +0900

    updata: Update ARwriteContents

--- components/AlcoholRecipe/ARWriteContents/ARWriteContents.tsx
+++ components/AlcoholRecipe/ARWriteContents/ARWriteContents.tsx
@@ -1,45 +1,103 @@
-import { faCameraRetro } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRotateBack, faCameraRetro, faTrashAlt, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { ChangeEvent, useCallback, useRef, useState } from 'react';
+import React, { ChangeEvent, ChangeEventHandler, KeyboardEventHandler, MouseEventHandler, useCallback, useRef, useState } from 'react';
 import styles from './ARWriteContents.module.scss'
 
 
 const ARWriteContents = () => {
    
-    const fileInput = React.useRef<HTMLInputElement >(null);
+    const fileInput = React.useRef<HTMLInputElement>(null);
+    const [file, setFile] = useState("");
+    
+    const [hashtag, setHashtag] = useState<string | ''>('');
+    const [hashArr, setHashArr] = useState<string[] | []>([]);
 
+    const [cocktailTitle , setRecipeTitle] = useState<string>('');
+    const [recipeContent, setRecipeContent] = useState<string>('');
+    const [cocktailConent , setcocktailConent] = useState<string>('');
+
+    const [thumbnailCheck, setThumbnailCheck] = useState<boolean>(false);
 
     const onClickUpload = () => {
-
         fileInput.current?.click();
-      
     }
 
-    const onChangeHashtag = (event : ChangeEvent) => {
+    const saveImageUpload = (e : ChangeEvent<HTMLInputElement>) =>{
+        if (e.target.files && e.target.files[0]) {
+            setFile(URL.createObjectURL(e.target.files[0]));
+            setThumbnailCheck(true);
+        }
     }
 
+    const deleteFileImage = () =>{
+            setFile("");
+            setThumbnailCheck(false);
+    };
 
+    const recipeNameOnChange = (e : ChangeEvent<HTMLInputElement>) => {
+        setRecipeTitle(e.target.value);
+    }
 
+    const hashtagInputChange = (e : ChangeEvent<HTMLInputElement>) => {
+        setHashtag(e.target.value.trim());
+        console.log(hashtag)
+    }
+    
+    const addHashTagKeyUp = useCallback((e :React.KeyboardEvent<HTMLInputElement>) =>{
+        if (e.currentTarget.value.trim() !=="" && e.key ==="Enter"){
+            if(hashArr.length < 4){
+                setHashArr((hashArr) => [...hashArr, hashtag]);
+                setHashtag("");
+            }else{
+                alert("최대 4개 까지 작성 가능합니다.");
+            }
+           
+        }
+        
+    },[hashtag, hashArr])
+    
+    const removeHashTag = (e:React.MouseEvent<HTMLButtonElement>) => {
+        setHashArr(hashArr.filter(hashtag => e.target !== hashtag));
+    }
 
     return(
         <div className={styles.writeContainer}>
-            <div className={styles.thumbnailImgBox}>
-                <h1>대표 사진</h1>
-                <button className={styles.thumbnailImgbtn} onClick={onClickUpload}>
-                    <div className={styles.thumbnailDiv} >
-                        <p><FontAwesomeIcon icon={faCameraRetro}/></p>
-                        <h1>메인 사진 올리기</h1>
-                        <span>( * 최대 1장 )</span>
-                    </div>
+            {!thumbnailCheck &&
+                <div className={styles.thumbnailInputBox}>
+                    <h1>대표 사진</h1>
+                    <button className={styles.thumbnailImgbtn} onClick={onClickUpload}>
+                        <div className={styles.thumbnailDiv} >
+                            <p><FontAwesomeIcon icon={faCameraRetro}/></p>
+                            <h1>메인 사진 올리기</h1>
+                            <span>( * 최대 1장 )</span>
+                        </div>
+                    </button>
+                </div>
+            }
+            
+            <div className={styles.imgPreviewBox}>
+                {thumbnailCheck &&<h1>대표 사진</h1>}
+                <div className={styles.imgPreview}>
                     <input
                         ref={fileInput}
                         type="file"
                         style={{display:"none"}}
                         multiple={false}
+                        accept="image/*,.pdf"
+                        onChange={saveImageUpload}
                     />
-                </button>
-                <div>
-                    <img src ="" alt=""></img>
+                    {file && <img src={file} />}
+
+                    {thumbnailCheck && 
+                        <div className={styles.imgTool}>
+                            <button onClick={deleteFileImage}>
+                                <FontAwesomeIcon icon={faTrashAlt}/>
+                            </button>
+                            <button onClick={onClickUpload}>
+                                <FontAwesomeIcon icon={faArrowRotateBack}/>
+                            </button>
+                        </div>
+                    }
                 </div>
             </div>
             <div className={styles.titleBox}>
@@ -47,26 +105,39 @@ const ARWriteContents = () => {
                 <input 
                     type="text"
                     placeholder="칵테일 이름 작성해주세요."
+                    onChange={recipeNameOnChange}
                 />
             </div>
             <div className={styles.titleBox}>
                 <h1>특징 태그 <span>(최대 4개)</span></h1>
                 <input 
                     type="text" 
-                    placeholder='tag를 입력 후,Enter를 누르세요'   
+                    value={hashtag}
+                    placeholder='tag를 입력 후,Enter를 누르세요'  
+                    onChange={hashtagInputChange} 
+                    onKeyUp={addHashTagKeyUp}
                 />
+                
+                <div className={styles.hashTagBox}>
+                    {hashArr && hashArr.map((item, i) => (
+                        <button key={i} onClick={removeHashTag}>
+                            <span>{item}</span>
+                            <span><FontAwesomeIcon icon={faXmark}/></span>
+                        </button>
+                    ))}
+                </div>
             </div>
             <div className={styles.editorbox}>
                 <h1>술 설명 </h1>
                 <textarea
-                    name="memo"
+                    name="cocktailMemo"
                     placeholder="술 설명"
                 />
             </div>
             <div className={styles.editorbox}>
                 <h1>레시피 <span>*</span></h1>
                 <textarea
-                    name="memo"
+                    name="RecipeMemo"
                     placeholder="레시피 설명"
                 />
             </div>
@@ -75,43 +146,3 @@ const ARWriteContents = () => {
 };
 
 export default ARWriteContents;
-
-/*
-    const [hashtag, setHashtag] = useState<string | ''>('');
-    const [hashArr, setHashArr] = useState<string[] | []>([]);
-
-    const [recipeTitle , setRecipeTitle] = useState<string>('');
-    const [recipeContent, setRecipeContent] = useState<string>('');
-
-    const selectFile = React.createRef();
-
-    const createHashTag=(event: KeyboardEvent)=>{
-        console.log(event.target);
-    }
-    const createHashTag = useCallback(
-        (event:KeyboardEvent) => {
-
-            if (process.browser) {
-            const $HashWrapOuter = document.querySelector('.HashWrapOuter')
-            const $HashWrapInner = document.createElement('div')
-            $HashWrapInner.className = 'HashWrapInner'
-            
-            $HashWrapInner.addEventListener('click', () => {
-                $HashWrapOuter?.removeChild($HashWrapInner)
-                console.log($HashWrapInner.innerHTML)
-                setHashArr(hashArr.filter((hashtag) => hashtag))
-            })
-    
-             enter 키 코드 :13 
-            if (event.keyCode === 13 && event.target.value.trim() !== '') {
-                console.log('Enter Key 입력됨!', e.target.value)
-                $HashWrapInner.innerHTML = '#' + e.target.value
-                $HashWrapOuter?.appendChild($HashWrapInner)
-                setHashArr((hashArr) => [...hashArr, hashtag])
-                setHashtag('')
-            }
-            
-            }
-        },[hashtag, hashArr]
-    )
-    **/
