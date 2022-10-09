8dfb079fce4c4ac44f0b8fb95a5e19c5ebe42e49
subin164 <qkrtnqls5669@gmail.com>
2022-10-09 19:56:18 +0900

    style : Change footer

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -7,9 +7,7 @@ const Layout: React.FC<{ children: JSX.Element | JSX.Element[] }> = ({ children 
     <>
       <AppHeader headerType="default" />
       <main>{children}</main>
-      <footer className={styles.footer}>
-        <AppFooter/>
-      </footer>
+      <AppFooter/>
     </>
   );
 };
