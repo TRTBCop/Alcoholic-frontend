92fdb7b8e3781616de0d6029646ea60ece61165b
seunghunHan1 <hkgb0009@gmail.com>
2022-10-10 15:53:23 +0900

    [COMMON] firebase 설정 값 env 세팅으로 변경 작업

--- components/LoginForm/LoginForm.tsx
+++ components/LoginForm/LoginForm.tsx
@@ -16,7 +16,6 @@ const LoginForm: React.FC = () => {
 
       const accessToken = result.user.stsTokenManager.accessToken;
       const refreshToken = result.user.stsTokenManager.refreshToken;
-      console.log(result, 123);
       setToken(accessToken, refreshToken);
 
       if (accessToken) {
--- plugins/firebase/__init__.ts
+++ plugins/firebase/__init__.ts
@@ -1,8 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import firebaseConfig from '../../firebase.json';
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp({
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.FB_AUTH_DOMAIN,
+  projectId: process.env.FB_PROJECT_ID,
+  storageBucket: process.env.FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+  appId: process.env.FB_APP_ID,
+});
 
 export const authService = getAuth(app);
