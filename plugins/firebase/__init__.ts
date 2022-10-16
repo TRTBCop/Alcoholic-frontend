b28a0b7945c0cd7775e0971abc5ca84074b38ae1
seunghunHan1 <hkgb0009@gmail.com>
2022-10-16 11:32:27 +0900

    [COMMON] env 세팅 값 없으면 파이어베이스 세팅 값 default 값 들어가게 수정

--- plugins/firebase/__init__.ts
+++ plugins/firebase/__init__.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 
 // Initialize Firebase
 const app = initializeApp({
-  apiKey: process.env.FB_API_KEY,
-  authDomain: process.env.FB_AUTH_DOMAIN,
-  projectId: process.env.FB_PROJECT_ID,
-  storageBucket: process.env.FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
-  appId: process.env.FB_APP_ID,
+  apiKey: process.env.FB_API_KEY || 'test',
+  authDomain: process.env.FB_AUTH_DOMAIN || 'test',
+  projectId: process.env.FB_PROJECT_ID || 'test',
+  storageBucket: process.env.FB_STORAGE_BUCKET || 'test',
+  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID || 'test',
+  appId: process.env.FB_APP_ID || 'test',
 });
 
 export const authService = getAuth(app);
